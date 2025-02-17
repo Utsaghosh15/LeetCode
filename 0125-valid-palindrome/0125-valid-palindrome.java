6505a20c7d724a8a01a71923ea011ac6667ca4fe
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-02-17 11:09:06 -0500

    Time: 19 ms (13.29%), Space: 45.1 MB (25.19%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+     s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); 
+     char[] sArray = s.toCharArray();
+     int start = 0;
+     int end = sArray.length-1;
+
+     while(start < end){
+
+      if(sArray[start] != sArray[end]){
+       System.out.println(sArray[start]+"__"+sArray[end]);
+       return false;
+      }
+
+      start++;
+      end--;   
+     }
+
+     return true;   
+    }
+}
