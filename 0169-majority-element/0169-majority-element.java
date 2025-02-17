3886d53daf6ac9e55823351c10c6a30b242c6404
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-02-17 00:03:32 -0500

    Time: 1 ms (99.82%), Space: 53.7 MB (8.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -61,22 +61,19 @@ class Solution {
     
     // return result;
 
-     int count = -1;
+     int count = 0;
      int result = 0;
 
      for(int i=0;i<nums.length;i++){
 
-      if(nums[i] == result){
-       count++;
-      }
-      else{
-       count-=1; 
-
-       if(count < 0){
+      if(count == 0){
         result = nums[i];
-        count = 1;
-       } 
       }
+
+      if(nums[i] == result)
+        count++;
+      else
+        count--;  
      }
 
      return result;
