efc9f7671bb16fd05247e6c38e1a9b98f39d029e
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-02-27 08:30:11 -0500

    Time: 1 ms (92.93%), Space: 44.7 MB (46.17%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public void Permutation(int[] nums, List<List<Integer>> result, List<Integer> sol,boolean[] used){
+
+      if(nums.length == sol.size()){
+        result.add(new ArrayList<>(sol));
+        return;
+      }  
+
+      for(int i=0;i<nums.length;i++){
+        
+       if(used[i])
+         continue;
+
+
+       sol.add(nums[i]);
+       used[i] = true;
+       Permutation(nums,result,sol,used);
+       
+       sol.remove(sol.size()-1);
+       used[i] = false; 
+      }
+    } 
+
+    public List<List<Integer>> permute(int[] nums) {
+
+     List<List<Integer>> result = new ArrayList<>();
+     boolean[] used = new boolean[nums.length];
+
+     Permutation(nums,result, new ArrayList<>(),used);
+
+     return result;
+    }
+}
