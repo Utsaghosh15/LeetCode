85ec8261aa6aaa2b8ce2e510b11a8c28463b6936
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-07-30 09:33:36 -0400

    Time: 1 ms (71%), Space: 42.5 MB (8.63%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,39 +14,45 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
+    
+    public void rightSide(TreeNode node,List<Integer> result){
 
-      List<Integer> result = new ArrayList<>();
+     if(node == null)
+      return; 
 
-      if(root == null)
-       return result; 
+     Queue<TreeNode> queue = new LinkedList<>();
+     queue.add(node);
+     int level_count = queue.size();
 
-      Queue<TreeNode> queue = new LinkedList<>();
-      queue.add(root);
-      int level_count = queue.size();
+     while(!queue.isEmpty()){
 
-      while(!queue.isEmpty()){
+      while(level_count > 0){
 
-        while(level_count != 0){
+        TreeNode temp = queue.poll();
 
-          TreeNode temp = queue.poll();
+        if(level_count == 1)
+         result.add(temp.val);
 
-          if(temp.left != null)
-            queue.add(temp.left);
+        if(temp.left != null)
+          queue.add(temp.left);
 
-          if(temp.right != null)
-            queue.add(temp.right);
+        if(temp.right != null)
+          queue.add(temp.right);
 
-          if(level_count == 1){
-            result.add(temp.val);
-          } 
-          
-          level_count--;      
-        }
-
-        level_count = queue.size();
+        level_count-=1;    
       }
 
-      return result;
+      if(level_count == 0)
+        level_count = queue.size();
+     }
+    }
+    
+    public List<Integer> rightSideView(TreeNode root) {
+
+     List<Integer> result = new ArrayList<>();
+
+     rightSide(root,result);
+
+     return result;
     }
 }
