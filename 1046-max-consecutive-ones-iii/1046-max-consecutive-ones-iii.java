72707c283f44e81dda419012a31957e5fa3a9c25
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-08-01 00:41:38 -0400

    Time: 3 ms (93.83%), Space: 53 MB (6.83%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -3,30 +3,29 @@ class Solution {
 
      int start = 0;
      int end = 0;
-     int zero = 0;
-     int maxlen = 0;
+     int current_k = 0;
+     int result = 0;
 
      while(end < nums.length){
 
       if(nums[end] == 0){
-        zero+=1;
+
+       current_k+=1;
+
+        while(current_k > k){
+
+         if(nums[start] == 0)
+          current_k-=1;
+
+         start+=1;    
+        }
+       
       }
 
-      while(zero > k){
-
-        if(nums[start] == 0) 
-           zero-=1;
-
-        start+=1;    
-      }
-      
-      if(zero <= k)
-       maxlen = Math.max(maxlen,end-start+1);
-
-      end+=1; 
-  
+      result = Math.max(result,end-start+1);
+      end+=1;  
      }
 
-     return maxlen;   
-    }   
+     return result;   
+    }
 }
