2ae512ce8aac2dfcf880c3701355f9aba55c1b0e
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-08-02 01:14:19 -0400

    Time: 11 ms (89.58%), Space: 44.7 MB (64.95%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -3,30 +3,20 @@ class Solution {
 
 
      int end = 0;
-     int start = 0;
      int result = 0;
-     char[] chars = s.toCharArray();
-     HashMap<Character,Integer> map = new HashMap<>();
+     int[] arr = {-1,-1,-1};
 
 
-     while(end < chars.length){
+     while(end < s.length()){
+  
+      char ch = s.charAt(end);
+      arr[ch - 'a'] = end;
 
-      map.put(chars[end],map.getOrDefault(chars[end],0)+1);
+      if(arr[0] != -1 && arr[1] != -1 && arr[2] != -1)
+        result += 1 + Math.min(arr[0],Math.min(arr[1],arr[2]));
 
-      while(map.size() == 3){
-       
-       result+= chars.length-end;
-
-       map.put(chars[start],map.get(chars[start])-1);
-
-       if(map.get(chars[start]) == 0)
-        map.remove(chars[start]);
-       
-       start+=1;
-      }
-
-      end+=1; 
-    }
+      end+=1;
+     } 
     
     return result;  
   }
