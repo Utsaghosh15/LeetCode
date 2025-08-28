4f17e20d44221630227975c942d31dd5a5758567
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-08-28 12:41:49 -0400

    Time: 3 ms (88.65%), Space: 44.3 MB (98.67%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,47 +1,46 @@
+import java.util.*;
+
 class Solution {
     public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() < t.length()) return "";
 
-     if (s.length() < t.length()) return "";
+        // Frequency of required chars from t
+        int[] need = new int[128];
+        int required = 0; // total number of chars we still need (by count)
 
-        Map<Character, Integer> map = new HashMap<>();
         for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            if (need[c] == 0) { /* just counting unique isn't enough; we use 'required' differently below */ }
+            need[c]++;
+            required++; // count total needed characters (with multiplicity)
         }
 
-        int start = 0;
-        int minLen = Integer.MAX_VALUE;
-        int minStart = 0;
-        int count = t.length();
+        int left = 0, right = 0;
+        int minLen = Integer.MAX_VALUE, minL = 0;
+        // We'll decrease 'required' as we satisfy characters and increase it when we drop needed characters.
+        while (right < s.length()) {
+            char rc = s.charAt(right);
+            // If this char was needed (need[rc] > 0), consuming it reduces 'required'
+            if (need[rc] > 0) required--;
+            // Whether needed or not, we mark it consumed by decreasing need[rc]
+            need[rc]--;
+            right++;
 
-        int i = 0, j = 0;
-        while (j < s.length()) {
-            char endChar = s.charAt(j);
-            if (map.containsKey(endChar)) {
-                if (map.get(endChar) > 0) {
-                    count--;
+            // When required == 0, window [left, right) contains all of t
+            while (required == 0) {
+                // Update best answer
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    minL = left;
                 }
-                map.put(endChar, map.get(endChar) - 1);
+                // Try to shrink from the left
+                char lc = s.charAt(left);
+                need[lc]++; // put back lc
+                // If after putting back, we now need lc (>0), window lost a required char
+                if (need[lc] > 0) required++;
+                left++;
             }
-
-            // Contract the window as much as possible while it's still valid
-            while (count == 0) {
-                if (j - i + 1 < minLen) {
-                    minLen = j - i + 1;
-                    minStart = i;
-                }
-
-                char startChar = s.charAt(i);
-                if (map.containsKey(startChar)) {
-                    map.put(startChar, map.get(startChar) + 1);
-                    if (map.get(startChar) > 0) {
-                        count++;
-                    }
-                }
-                i++;
-            }
-            j++;
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minL, minL + minLen);
     }
 }
