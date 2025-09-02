08e50c3637d8ddaf58067cc7002106634d941273
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-09-02 12:03:37 -0400

    Time: 2 ms (66.69%), Space: 42.4 MB (76.08%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,73 +1,69 @@
-class Pair{
 
- int first;
- int second;
- int time;
-
- Pair(int first,int second,int time){
-
-  this.first = first;
-  this.second = second;
-  this.time = time;  
- }   
-}
 class Solution {
+
+    class Pair{
+
+     int first;
+     int second; 
+     int time;  
+
+     Pair(int first,int second,int time){
+      this.first = first;
+      this.second = second;
+      this.time = time;
+     }   
+    }
+
     public int orangesRotting(int[][] grid) {
 
-     int cntFresh = 0;
-     Queue<Pair> queue = new LinkedList<Pair>();
-     int[][] visited_array = new int[grid.length][grid[0].length];
+    boolean[][] visited_array = new boolean[grid.length][grid[0].length]; 
+    Queue<Pair> queue = new ArrayDeque<>();
+    int cntFresh = 0;
+    int cnt = 0;
+    int maxTime = 0;
+    int[] row_array = {-1,0,1,0}; 
+    int[] col_array = {0,1,0,-1}; 
+    int row_length = grid.length; 
+    int col_length = grid[0].length; 
 
-     for(int i=0;i<grid.length;i++){
-      for(int j=0;j<grid[0].length;j++){
+    
+    for(int row=0;row<grid.length;row++){ 
+        for(int col=0;col<grid[0].length;col++){ 
+            if(grid[row][col] == 2 && visited_array[row][col] == false){ 
+              queue.add(new Pair(row,col,0));
+              visited_array[row][col] = true;
+            }
 
-        if(grid[i][j] == 2){
-          queue.add(new Pair(i,j,0));
-          visited_array[i][j] = 2;  
-        }
-        else{
-          visited_array[i][j] = 0;  
-        }
+            if(grid[row][col] == 1)
+             cntFresh+=1; 
+        } 
+    }
 
-        if(grid[i][j] == 1)
-         cntFresh++;
-      }  
-     }
-
-
-     int result = 0;
-     int cnt = 0;
-     int[] row_array = {-1,0,1,0};
-     int[] col_array = {0,1,0,-1};
-
-
-     while(!queue.isEmpty()){
-
+    while(!queue.isEmpty()){
+      
       int temp_row = queue.peek().first;
       int temp_col = queue.peek().second;
       int temp_time = queue.peek().time;
       queue.remove();
 
-      result = Math.max(result,temp_time);
-
+      maxTime = Math.max(maxTime,temp_time);
+      
       for(int i=0;i<row_array.length;i++){
-        
+
         int nrow = temp_row+row_array[i];
         int ncol = temp_col+col_array[i];
 
-        if(nrow >= 0 && nrow < grid.length && ncol >= 0 && ncol < grid[0].length && visited_array[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-
-         visited_array[nrow][ncol] = 2;
-         cnt++;
-         queue.add(new Pair(nrow,ncol,temp_time+1));   
+        if(nrow >= 0 && nrow < row_length && ncol >= 0 && ncol < col_length && grid[nrow][ncol] == 1 && visited_array[nrow][ncol] == false){
+          visited_array[nrow][ncol] = true;
+          queue.add(new Pair(nrow,ncol,temp_time+1)); 
+          cnt+=1; 
         }
-
-      }       
+      }
      }
 
-      if(cnt != cntFresh)
-        return -1;
-
-      return result;  
+     if(cnt != cntFresh)
+      return -1; 
+    
+    return maxTime;
     }
 }
