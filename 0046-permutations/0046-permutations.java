e8e7745488bad3b354628be0ac465fb9ca541a9f
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2025-10-16 11:45:56 -0400

    Time: 1 ms (93.02%), Space: 44.4 MB (82.98%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,37 +1,33 @@
 class Solution {
+    
+    public void permutation(int[] nums, boolean[] used, List<Integer> list,List<List<Integer>> result){
 
-    public void Permutation(int[] nums,List<List<Integer>> result,List<Integer> sol,boolean[] used){
-
-     if(nums.length == sol.size()){
-       result.add(new ArrayList<>(sol));
-       return; 
+     if(list.size() == nums.length){
+      result.add(new ArrayList<>(list));
+      return;  
      }   
 
      for(int i=0;i<nums.length;i++){
 
-       if(used[i])
-        continue;
+      if(used[i])
+       continue;
 
-       sol.add(nums[i]);
-       used[i] = true;
+      list.add(nums[i]);
+      used[i] = true;
+      permutation(nums,used,list,result);
+      list.remove(list.size()-1); 
+      used[i] = false;
+     } 
 
-       Permutation(nums,result,sol,used);
-
-       sol.remove(sol.size()-1);
-       used[i] = false;  
-     }
     }
-
-
+    
     public List<List<Integer>> permute(int[] nums) {
 
+     List<List<Integer>> result = new ArrayList<>();
+     boolean[] used = new boolean[nums.length];
+     
+     permutation(nums,used,new ArrayList<>(),result);
 
-        List<List<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-
-        Permutation(nums,result,new ArrayList<>(),used);
-
-        return result;
-
+     return result;   
     }
 }
