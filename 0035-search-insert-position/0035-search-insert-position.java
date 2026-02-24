aa217c55b42ff72eae1d7936718df98060471426
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-02-24 17:31:31 -0500

    Time: 0 ms (100%), Space: 44.5 MB (96.55%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,23 +1,25 @@
 class Solution {
-
-    public int BinarySearch(int[] nums,int low,int high,int target){
-
-     if(low > high)
-      return low; 
-
-     int mid = low + (high-low)/2;
-
-     if(nums[mid] == target)
-      return mid;
-     else if(nums[mid] > target)
-      return BinarySearch(nums,low,mid-1,target);
-     else 
-      return BinarySearch(nums,mid+1,high,target);
-           
-    }
-
     public int searchInsert(int[] nums, int target) {
 
-     return BinarySearch(nums,0,nums.length-1,target);    
+
+     int low = 0;
+     int high = nums.length-1;
+
+     while(low<=high){
+      
+      int mid = low + (high-low)/2;
+
+      if(nums[mid] == target)
+       return mid;
+
+      if(nums[mid] < target){
+        low = mid+1;
+      }
+      else{
+        high = mid-1;
+      }
+     }
+
+     return low;  
     }
 }
