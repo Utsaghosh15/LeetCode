b62ffb2473228deb6a5f46fcaaa1fe3b55bd2626
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-04-05 10:29:11 -0400

    Time: 33 ms (73.49%), Space: 58.6 MB (94.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,29 +4,28 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
-            
-            // Skip duplicate values for the 'start' element
+
+            // Skip duplicate for i
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1;               // ✅ start left AFTER i
+            int left = i + 1;
             int right = nums.length - 1;
+            int target = -nums[i];  // we need left+right == target
 
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+                int sum = nums[left] + nums[right];
 
-                if (sum == 0) {
+                if (sum == target) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Skip duplicates for left and right
+                    // Skip duplicates before moving
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
-
                     left++;
                     right--;
-                } else if (sum < 0) {
-                    left++;     // need larger sum
+                } else if (sum < target) {
+                    left++;
                 } else {
-                    right--;    // need smaller sum
+                    right--;
                 }
             }
         }
