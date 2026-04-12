a03274b13b60483f3bb66edc6d9025f1bc62fc43
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-04-11 22:46:19 -0400

    Time: 8 ms (91.31%), Space: 49.3 MB (16.52%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,40 +1,35 @@
 class Solution {
     public int[][] merge(int[] [] intervals) {
 
-        // Step 1: sort by start
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+      Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
 
-        int[][] result = new int[intervals.length][2];
-        int idx = 0;
+      int[][] result = new int[intervals.length][intervals[0].length];
+      int idx = 0; 
 
-        // Step 2: take first interval as current
-        int start = intervals[0][0];
-        int end   = intervals[0][1];
+      int start = intervals[0][0];
+      int end = intervals[0][1];
 
-        for(int i = 1; i < intervals.length; i++){
+      for(int i=1;i<intervals.length;i++){
+       
+       if(intervals[i][0] <= end){
+        
+        end = Math.max(end,intervals[i][1]);
+       }
+       else{
+         
+         result[idx][0] = start;
+         result[idx][1] = end;
+         idx+=1;
 
-            if(intervals[i][0] <= end){
-                // OVERLAP → extend end if needed
-                end = Math.max(end, intervals[i][1]);
-            }
-            else{
-                // NO OVERLAP → save current, move forward
-                result[idx][0] = start;
-                result[idx][1] = end;
-                idx++;
+         start = intervals[i][0];
+         end = intervals[i][1];
+       }
+      }
 
-                // new current interval
-                start = intervals[i][0];
-                end   = intervals[i][1];
-            }
-        }
+      result[idx][0] = start; 
+      result[idx][1] = end; 
+      idx+=1;
 
-        // Step 3: add last interval
-        result[idx][0] = start;
-        result[idx][1] = end;
-        idx++;
-
-        // Step 4: trim result to actual size
-        return Arrays.copyOfRange(result, 0, idx);
+      return Arrays.copyOfRange(result,0,idx);
     }
 }
