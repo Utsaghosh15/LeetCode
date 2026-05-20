d5a5607df6ffecb973b878bb8baf672c397f03f7
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-05-20 16:08:56 -0400

    Time: 10 ms (25.41%), Space: 41.3 MB (99.8%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public int fibonacci(int n){
+
+     if(n == 0)
+      return 0;
+
+     if(n == 1)
+      return 1;
+
+     return fibonacci(n-1) + fibonacci(n-2);     
+    }
+    
+    public int fib(int n) {
+
+     if(n == 0)
+      return 0;
+
+     return fibonacci(n);    
+    }
+}
