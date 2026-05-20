d7b6570be998f3a2c0c39d10d23a50f3419c7530
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-05-20 19:38:01 -0400

    Time: 0 ms (100%), Space: 42.2 MB (15.85%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,27 +1,39 @@
 class Solution {
     
-    // public int fibonacci(int n,int[] result){
+    // Tabulation 
+    public int fibonacci(int n,int[] result){
 
     //  result[0] = 0;       
     //  result[1] = 1;
      
-    //  for(int i=2;i<=n;i++){
-    //   result[i] = result[i-1] + result[i-2];
-    //  }
-     
-    //  return result[n];     
-    // }
-
-    public int fibonacci(int n,int[] result){
-
      if(n<=1)
       return n;
 
-     if(result[n] != 0)
-      return result[n];
-
-     return result[n] = fibonacci(n-1,result) + fibonacci(n-2,result);    
+     int last = 0;
+     int second_last = 1;
+     int ans = 0;
+     
+     for(int i=2;i<=n;i++){  
+      ans = second_last + last;
+      last = second_last;
+      second_last = ans; 
+     }
+     
+    //  return result[n];
+     return ans;     
     }
+
+    // Memorization
+    // public int fibonacci(int n,int[] result){
+
+    //  if(n<=1)
+    //   return n;
+
+    //  if(result[n] != 0)
+    //   return result[n];
+
+    //  return result[n] = fibonacci(n-1,result) + fibonacci(n-2,result);    
+    // }
     
     public int fib(int n) {
 
