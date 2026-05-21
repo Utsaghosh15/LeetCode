52bb546d5234de9614ba54dc39a50320264d382f
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-05-20 20:54:08 -0400

    Time: 0 ms (100%), Space: 42.3 MB (16.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,17 +7,33 @@ class Solution {
      
     //  return climbStair(n-1) + climbStair(n-2);
     // }
+   
+    // Memorization
+    // public int climbStair(int n,int[] result){
 
+    //  if(n <= 1)
+    //   return 1;
+
+    //  if(result[n] != 0)
+    //   return result[n]; 
+     
+    //  return result[n] = climbStair(n-1,result) + climbStair(n-2,result);
+    // }
+ 
+
+    // Tabulation
     public int climbStair(int n,int[] result){
 
-     if(n <= 1)
-      return 1;
+     result[0] = 1;
+     result[1] = 1;
 
-     if(result[n] != 0)
-      return result[n]; 
-     
-     return result[n] = climbStair(n-1,result) + climbStair(n-2,result);
+     for(int i=2;i<=n;i++){
+      result[i] = result[i-1] + result[i-2];
+     }
+
+     return result[n];   
     }
+
 
     public int climbStairs(int n) {
 
