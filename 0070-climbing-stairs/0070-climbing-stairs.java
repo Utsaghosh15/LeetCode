c69a96c19e2d7234845653828303556b74ecd61f
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-05-20 20:58:11 -0400

    Time: 0 ms (100%), Space: 41.4 MB (98.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -24,14 +24,26 @@ class Solution {
     // Tabulation
     public int climbStair(int n,int[] result){
 
-     result[0] = 1;
-     result[1] = 1;
+     if(n<=1)
+      return 1;
+
+    //  result[0] = 1;
+    //  result[1] = 1;
+
+     int last = 1;
+     int second_last = 1;
+     int ans = 0;
 
      for(int i=2;i<=n;i++){
       result[i] = result[i-1] + result[i-2];
+
+      ans = last+second_last;
+      last = second_last;
+      second_last = ans;
      }
 
-     return result[n];   
+    //  return result[n]; 
+     return ans;  
     }
 
 
