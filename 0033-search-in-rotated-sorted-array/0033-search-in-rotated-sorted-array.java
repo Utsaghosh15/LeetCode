b2be562e013c1a14fb182bf66925571f3e131db2
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-06-02 14:20:14 -0400

    Time: 0 ms (100%), Space: 44 MB (26.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,31 +1,34 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;  // bug fix: should be length-1, not length
+     
+     int low = 0;
+     int high = nums.length-1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+     while(low <= high){
+      
+      int mid = low + (high-low)/2;
 
-            if (nums[mid] == target) return mid;
+      if(nums[mid] == target)
+       return mid;
 
-            // Left half is sorted
-            if (nums[low] <= nums[mid]) {
-                if (target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;  // target is in left half
-                } else {
-                    low = mid + 1;   // target is in right half
-                }
-            }
-            // Right half is sorted
-            else {
-                if (target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;   // target is in right half
-                } else {
-                    high = mid - 1;  // target is in left half
-                }
-            }
+      if(nums[low] <= nums[mid]){ 
+        if(target >= nums[low] && target < nums[mid]){
+         high = mid-1;
         }
+        else{
+            low = mid+1;
+        }
+      }
+      else{
+        if(target <= nums[high] && target > nums[mid]){
+         low = mid+1;
+        }
+        else{
+         high = mid-1;
+        }
+      } 
+     }
 
-        return -1;
+     return -1;
     }
 }
