ad835eb0bcba91902005ed6c65125010cbfcfac4
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-06-11 13:16:06 -0400

    Time: 2 ms (76.35%), Space: 47.6 MB (72.32%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,52 +1,55 @@
 class Solution {
 
-   public void DFS(int row,int col,boolean[][] visited_array,char[][] isConnected){ 
+    public void DFS(int row, int col, char[][] board, boolean[][] visited_array){  
+
+     visited_array[row][col] = true;
+     int[] row_array = {-1,0,1,0};
+     int[] col_array = {0,1,0,-1};
+
+      for(int i=0;i<row_array.length;i++){
+
+       int nrow = row + row_array[i];
+       int ncol = col + col_array[i]; 
+
+       if(nrow >= 0 && nrow < board.length && ncol >= 0 && ncol < board[0].length && board[nrow][ncol] == 'O' && visited_array[nrow][ncol] == false){
+        DFS(nrow,ncol,board,visited_array); 
+       } 
+      }      
+     }
     
-    visited_array[row][col] = true; 
-    int[] row_array = {-1,0,1,0}; 
-    int[] col_array = {0,1,0,-1}; 
-    int row_length = isConnected.length; 
-    int col_length = isConnected[0].length; 
 
-    for(int i=0;i<row_array.length;i++){ 
-      int nrow = row_array[i]+row; 
-      int ncol = col_array[i]+col; 
-
-      if(nrow >= 0 && nrow < row_length && ncol >= 0 && ncol < col_length && isConnected[nrow][ncol] == 'O' && visited_array[nrow][ncol] == false){ 
-        DFS(nrow,ncol,visited_array,isConnected); 
-      } 
-    }  
-   }
 
     public void solve(char[][] board) {
 
-     boolean[][] visited_array = new boolean[board.length][board[0].length];
-     int n = board.length;
-     int m = board[0].length;
+     boolean[][] visited_array = new boolean[board.length][board[0].length];   
 
-     for(int i=0;i<m;i++){
-       
-       if(board[0][i] == 'O' && visited_array[0][i] == false)
-        DFS(0,i,visited_array,board);
 
-       if(board[n-1][i] == 'O' && visited_array[n-1][i] == false)
-        DFS(n-1,i,visited_array,board);    
+     for(int i=0;i<board[0].length;i++){
+
+      if(board[0][i] == 'O' && visited_array[0][i] == false)
+        DFS(0,i,board,visited_array);
+
+      if(board[board.length-1][i] == 'O' && visited_array[board.length-1][i] == false)
+        DFS(board.length-1,i,board,visited_array);    
      }
 
-     for(int i=0;i<n;i++){
-       
-       if(board[i][0] == 'O' && visited_array[i][0] == false)
-        DFS(i,0,visited_array,board);
+     for(int i=0;i<board.length;i++){
 
-       if(board[i][m-1] == 'O' && visited_array[i][m-1] == false)
-        DFS(i,m-1,visited_array,board);    
+      if(board[i][0] == 'O' && visited_array[i][0] == false)
+       DFS(i,0,board,visited_array);
+
+      if(board[i][board[0].length-1] == 'O' && visited_array[i][board[0].length-1] == false)
+       DFS(i,board[0].length-1,board,visited_array);   
      }
- 
-      for(int i=0;i<board.length;i++){
-       for(int j=0;j<board[0].length;j++){
-        if(visited_array[i][j] == false && board[i][j] == 'O')
-         board[i][j] = 'X';
+     
+     for(int row=0;row < board.length;row++){
+       for(int col=0;col < board[0].length;col++){
+
+        if(visited_array[row][col] == false && board[row][col] == 'O'){
+         board[row][col] = 'X';
+        }
        } 
-      }
+     }
+
     }   
 }
