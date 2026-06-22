9cec8d388f6485bec7449772010404bb5aace3b8
Utsha Ghosh <100540030+Utsaghosh15@users.noreply.github.com>
2026-06-22 15:36:39 -0400

    Time: 0 ms (100%), Space: 42.8 MB (36.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,27 +1,29 @@
 class Solution {
 
-    public int HouseRob(int index, int[] nums, int[] dp){
+    public int HouseRob(int index, int freeWill, int[] nums, int[][] dp){
 
       if(index < 0)
        return 0;
 
-      if(index == 0)
-        return nums[index];
+      if(dp[index][freeWill] != -1)
+        return dp[index][freeWill];
 
-      if(dp[index] != -1)
-        return dp[index];
+      if(freeWill == 0)
+        return dp[index][freeWill] = HouseRob(index-1,1,nums,dp);
 
-      int amount1 = HouseRob(index-2,nums,dp) + nums[index];
-      int amount2 = HouseRob(index-1,nums,dp);
+      int amount1 = HouseRob(index-1,0,nums,dp) + nums[index];
+      int amount2 = HouseRob(index-1,1,nums,dp);
 
-      return dp[index] = Math.max(amount1,amount2);    
+      return dp[index][freeWill] = Math.max(amount1,amount2);    
     }
 
     public int rob(int[] nums) {
 
-     int[] dp = new int[nums.length]; 
-     Arrays.fill(dp, -1);
+     int[][] dp = new int[nums.length][2]; 
+
+     for(int[] dp_freeWill : dp)
+       Arrays.fill(dp_freeWill, -1);
    
-     return HouseRob(nums.length-1,nums,dp);   
+     return HouseRob(nums.length-1,1,nums,dp);   
     }
 }
